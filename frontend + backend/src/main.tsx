405b3947b67f5f0f3bfcc261e70b10370fe807d3
omkardinde04 <omkar.dinde@somaiya.edu>
2025-11-09 13:51:00 +0530

    dependencies plus updated css and main.tsx file

--- frontend + backend/src/main.tsx
+++ frontend + backend/src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ThemeProvider } from "./components/ThemeProvider";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+createRoot(document.getElementById("root")!).render(
+  <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
     <App />
-  </StrictMode>,
-)
+  </ThemeProvider>
+);
